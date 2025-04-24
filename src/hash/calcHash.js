a16a162745d697031975277b3e57cf1407541f62
potatosim <devildriver2310@gmail.com>
2025-04-25 01:51:09 +0300

    feat: implement function from hash block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = createReadStream(
+    path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'),
+    'utf-8'
+  );
+
+  const hash = createHash('sha256');
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
 };
 
 await calculateHash();
