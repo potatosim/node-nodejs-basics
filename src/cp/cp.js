2d44ffc20f9d0d50d0933fd392be95015d7bd67f
Hanna Yemelyanova <hanna.yemelyanova@corevist.com>
2025-04-28 18:33:25 +0300

    feat: implement spawnChildProcess fn from cp block

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from 'node:child_process';
+import { stdin, stdout } from 'node:process';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn(
+    'node',
+    [path.join(__dirname, 'files', 'script.js'), ...args],
+    {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+    }
+  );
+
+  stdin.pipe(child.stdin);
+  child.stdout.pipe(stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['example1', 2, false]);
